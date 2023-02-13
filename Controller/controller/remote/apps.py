d7835a39cda29d3ff57823ca3099cb3d0e604010
KwonSunJae <ksun4131@gmail.com>
2023-02-14 05:39:22 +0900

    [Init] Initial Commit

--- Controller/controller/remote/apps.py
+++ Controller/controller/remote/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RemoteBackConfig(AppConfig):
+class RemoteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'remote_back'
+    name = 'remote'
