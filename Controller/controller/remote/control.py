b1261c00582539c9ac8ae8d9a6607b9ac6da6822
KwonSunjae <ksun4131@gmail.com>
2023-02-19 19:55:40 +0900

    [test]

--- Controller/controller/remote/control.py
+++ Controller/controller/remote/control.py
@@ -3,8 +3,8 @@ import subprocess
 
 def initFog( remote):
     query = 'sshpass -p {} scp {} dmslab-sj@{}:~/'.format(remote.rootpw, "~/Autonomic-Fog-Comgputing-/SelfConfiguring/fog_setup.sh", remote.ip)
-    sc = subprocess.run(query)
+    sc = subprocess.run(query, shell= True)
 
 def initEdge( remote ):
     query = 'sshpass -p {} scp {} dmslab-sj@{}:~/'.format(remote.rootpw,"~/Autonomic-Fog-Comgputing-/SelfConfiguring/edge_setup.sh", remote.ip)
-    sc = subprocess.run(query)
+    sc = subprocess.run(query, shell= True)
