1a5d3cd2529c122c7cb4897dc19e6aea1b38f2a3
KwonSunJae <ksun4131@gmail.com>
2023-02-21 04:36:00 +0900

    [fix3]

--- Controller/controller/remote/urls.py
+++ Controller/controller/remote/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
-from .views import helloAPI , checkAPI , registerMachine
+from .views import helloAPI , checkAPI , registerMachine, automodeON, automodeOFF, edge2fog, fog2edge
 
 urlpatterns = [
     path("hello/",helloAPI),
     path("test/", checkAPI),
-    path("register/",registerMachine)
+    path("register/",registerMachine),
+    path("autoon/",automodeON),
+    path("autooff/",automodeOFF),
+    path("e2f/",edge2fog),
+    path("f2e/",fog2edge),
+    
+
+    
+
 ]
