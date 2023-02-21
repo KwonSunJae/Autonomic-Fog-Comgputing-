81e8eb4e2d11e4a78958d7a790531183e4f8a846
KwonSunJae <ksun4131@gmail.com>
2023-02-21 16:33:43 +0900

    [fix]

--- Controller/controller/remote/creater.py
+++ Controller/controller/remote/creater.py
@@ -6,7 +6,7 @@ def createDockerfile(modules,remote):
         subprocess.run("mkdir "+ module.name, shell= True)
         end = '\n'
         f = open( "./"+module.name+"/Dockerfile",mode='w' )
-        f.write("FROM ubuntu: 20.04\n")
+        f.write("FROM ubuntu:20.04\n")
         f.write("RUN apt-get update -y && apt-get upgrade -y\n")
         f.write("RUN apt-get install nginx git\n")
         f.write("RUN git clone " + module.giturl +end)
