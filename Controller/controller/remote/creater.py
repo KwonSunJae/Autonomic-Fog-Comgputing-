ad9e1a0aa52d102184878f1f6f84cfe60c1a18c4
KwonSunJae <ksun4131@gmail.com>
2023-02-21 15:54:55 +0900

    [fix]

--- Controller/controller/remote/creater.py
+++ Controller/controller/remote/creater.py
@@ -19,9 +19,9 @@ def createDockerfile(modules,remote):
         mers = module.execute
         f.write("CMD "+ mers + end)
         f.close()
-        query = 'sudo docker build -t ksun4131/' + str(module.remote_id.id) + module.name  + " ./" + module.name +"/"
+        query = 'cd {} && sudo docker build -t ksun4131/'.format(module.name) + str(module.remote_id.id) + module.name  + " ./" + module.name +"/"
         subprocess.run(query, shell= True)
-        query = "sudo docker pull ksun4131/{}".format(str(module.remote_id.id) +module.name)
+        query = "sudo docker push ksun4131/{}".format(str(module.remote_id.id) +module.name)
         subprocess.run(query, shell= True)
 def createYAMLfile(modules,remote):
     for module in modules:
