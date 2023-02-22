1718e7a296732b8760ff8379a55ca925e97d75ce
KwonSunJae <ksun4131@gmail.com>
2023-02-22 18:37:09 +0900

    [x

--- Controller/controller/remote/creater.py
+++ Controller/controller/remote/creater.py
@@ -72,7 +72,7 @@ spec:
 
 def runDockerfile(modules, remote):
     for module in modules:
-        print('is running')
+        print('is running'+ str(module.remote_id.id) +module.name)
         query = "sshpass -p {} ssh root@{} '{}'".format(remote.rootpw, remote.ip,  "docker pull ksun4131/{} && docker run -d --name {} ksun4131/{}".format(str(module.remote_id.id) +module.name ,module.name, str(module.remote_id.id) +module.name ))
         subprocess.run(query, shell= True)
 
