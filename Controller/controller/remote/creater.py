050b34581a71704cc1967a4f119428f36c9cac93
KwonSunJae <ksun4131@gmail.com>
2023-02-22 14:37:47 +0900

    [fix] error

--- Controller/controller/remote/creater.py
+++ Controller/controller/remote/creater.py
@@ -24,7 +24,7 @@ ExecStart= python3\\ \n\
 Restart=on-failure\n\
 [Install]\n\
 WantedBy=multi-user.target" >> /etc/systemd/system/autonomic.service \
-&& systemctl daemon-reload && systemctl enable autonomic.service'''.format(module.remote_id.ip, module.name)
+&& systemctl daemon-reload && systemctl enable autonomic.service\n'''.format(module.remote_id.ip, module.name)
         f.write(service)
         runs = 'RUN '
         print(module.install)
