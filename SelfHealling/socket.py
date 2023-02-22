614db97446c200d61c76fcac3b6fbfe89d73ef94
KwonSunJae <ksun4131@gmail.com>
2023-02-22 13:53:57 +0900

    [edit] socket.py

--- SelfHealling/socket.py
+++ SelfHealling/socket.py
@@ -1,9 +1,9 @@
 import socketio
 import time
-
+import sys
 sio = socketio.Client()
-sio.connect()
+sio.connect('http://api-mongy.bibim-bap.com/socket')
 
 while True:
     time.sleep(1)
-    sio.emit('my message', {'foo': 'bar'})
+    sio.emit(sys.argv[1]+' '+sys.argv[2])
